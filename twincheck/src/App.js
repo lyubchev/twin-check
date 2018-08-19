9f38c05f43d4c55e62866620f24ca87d724c0e1d
Lyubo Lyubchev <lyubo_2317@abv.bg>
2018-08-19 18:31:29 +0300

    Add Roboto font, Update header

--- twincheck/src/App.js
+++ twincheck/src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import FacePanel from './components/FacePanel.jsx';
+import './assets/fonts/style.css';
 import './assets/css/App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="wrapper">
-        <header>
-          c=3
-          <br />
-        </header>
+      <div className="container">
+        <h1>
+          <i className="far fa-check-circle"></i> twincheck!
+        </h1>
         <div className="grid">
           <div>
             <FacePanel />
